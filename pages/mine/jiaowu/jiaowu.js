530e637323af70063d99db141bb89b6aaa99a042
MilesTin <1154520245@qq.com>
2019-05-30 00:09:09 +0800

    学号绑定页面完成，有点小问题

--- pages/mine/jiaowu/jiaowu.js
+++ pages/mine/jiaowu/jiaowu.js
@@ -1,5 +1,7 @@
 // pages/mine/jiaowu/jiaowu.js
 var app = getApp();
+//页面重新加载有问题，验证码未重新加载
+
 Page({
 
   /**
@@ -11,27 +13,42 @@ Page({
     stuId:"",
     passwd:"",
   },
-
+  
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
     //请求加载验证码
-    var that = this;
-    let header = {'Content-Type':'application/json'};
-    header['Cookie'] = wx.getStorageSync('sessionid');
-    var localImgUrl = ""
-    wx.request({
-      // url: app.globalData.localUrl + "/account/getIdCaptcha",
-      url: app.globalData.url + "/account/getIdCaptcha",
-      header:header,
-      success:function(res){
-        that.setData({ "verifCodeUrl":"http://129.28.140.83:81/static/matesHelps/account/img/login.jpg"});
-        wx.setStorageSync("sessionid", res.header['Set-Cookie']);
+    function getImg(thisObj) {
+      var that = thisObj;
+      let header = { 'Content-Type': 'application/json' };
+      header['Cookie'] = wx.getStorageSync('sessionid');
+      var localImgUrl = ""
+      wx.request({
+        // url: app.globalData.localUrl + "/account/getIdCaptcha",
+        url: app.globalData.url + "/account/getIdCaptcha",
+        header: header,
+        success: function (res) {
+          sleep(100);
+          that.setData({ "verifCodeUrl": "http://129.28.140.83:81/static/matesHelps/account/img/login.jpg" });
+          wx.setStorageSync("sessionid", res.header['Set-Cookie']);
+        }
+      })
+    };
+    function sleep(n) {
+      var start = new Date().getTime();
+      //  console.log('休眠前：' + start);
+      while (true) {
+        if (new Date().getTime() - start > n) {
+          break;
+        }
       }
-    })
+      // console.log('休眠后：' + new Date().getTime());
+    };
+    
+    getImg(this);
   },
-
+  
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
@@ -64,7 +81,33 @@ Page({
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-
+    function sleep(n) {
+      var start = new Date().getTime();
+      //  console.log('休眠前：' + start);
+      while (true) {
+        if (new Date().getTime() - start > n) {
+          break;
+        }
+      }
+      // console.log('休眠后：' + new Date().getTime());
+    };
+    function getImg(thisObj) {
+      var that = thisObj;
+      let header = { 'Content-Type': 'application/json' };
+      header['Cookie'] = wx.getStorageSync('sessionid');
+      var localImgUrl = ""
+      wx.request({
+        // url: app.globalData.localUrl + "/account/getIdCaptcha",
+        url: app.globalData.url + "/account/getIdCaptcha",
+        header: header,
+        success: function (res) {
+          sleep(100);
+          that.setData({ "verifCodeUrl": "http://129.28.140.83:81/static/matesHelps/account/img/login.jpg" });
+          wx.setStorageSync("sessionid", res.header['Set-Cookie']);
+        }
+      })};
+      getImg(this);
+      setTimeout(function(){wx.stopPullDownRefresh();},1000);
   },
 
   /**
@@ -86,7 +129,7 @@ Page({
     let passwd = this.data.passwd;
     var that = this;
     let header = { "Content-Type": "application/x-www-form-urlencoded" };
-    let data = {"stuId":"2017141461248","passwd":"014170","captcha":captcha};
+    let data = {"stuId":stuId,"passwd":passwd,"captcha":captcha};
     console.log(data);
     header['Cookie'] = wx.getStorageSync("sessionid");
     wx.request({
@@ -97,6 +140,25 @@ Page({
       method:'POST',
       success:function(res){
         console.log(res);
+        if (res.statusCode==200){
+          wx.showToast({
+            title: '绑定成功',
+            success:function(res){
+              wx.navigateTo({
+                url: '../../mine/mine',
+              })
+            }
+          })
+          
+        }
+        else{
+          that.onPullDownRefresh();
+          that.onLoad();
+        }
+      },
+      fail:function(){
+        that.onPullDownRefresh();
+        that.onLoad();
       }
     })
   },
@@ -109,4 +171,5 @@ Page({
   stuIdInput:function(e){
     this.setData({ "stuId": e.detail.value });
   },
+ 
 })
