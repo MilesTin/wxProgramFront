0c861940619d5fb98a27634a6fa5429a826dd197
MilesTin <1154520245@qq.com>
2019-05-30 23:16:57 +0800

    modify some

--- pages/mine/mine.js
+++ pages/mine/mine.js
@@ -1,11 +1,12 @@
 // pages/mine/mine.js
+var app = getApp();
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    width:0,
+    'user':{},
   },
 
   /**
@@ -14,6 +15,24 @@ Page({
   onLoad: function (options) {
     this.setData({
       width:200
+    });
+    var that = this;
+    let header = {"Content-Type":"application/json"};
+    header['Cookie'] = wx.getStorageSync("sessionid");
+    wx.request({
+      url:app.globalData.localUrl + "/account/myInfo",
+      // url: app.globalData.url + "/account/myInfo',
+      header:header,
+      success:function(res){
+        console.log(typeof(res.data));
+        console.log(res.data);
+        let data = res.data;
+        data = data.split("'").join('"');
+        console.log(data)
+        data = JSON.parse(data);
+        that.setData({"user":data});
+      
+      }
     })
   },
 
@@ -84,5 +103,31 @@ Page({
     wx.navigateTo({
       url: 'setting/setting',
     })
+  },
+  rateStar(index) {
+    var star = document.getElementById('star');
+    var items = star.getElementsByTagName("li");
+    index = parseInt(index) - 1; //因为一个星星由2个a标签组成的,所以减掉1
+    var tem = -1;
+    //遍历所有的li标签
+    for(var i = 0; i<items.length; i++) {
+  if (index > i * 2) { //当index大于i*2的时候,就填充i个数量的满格星星
+    items[i].style.background = "url() no-repeat";
+    items[i].style.backgroundSize = "100% 100%";
+  } else {
+    if (tem == -1) {
+      tem = i;
+    }
+    items[i].style.background = "url() no-repeat"; //填充灰色状态的星星
+    items[i].style.backgroundSize = "100% 100%";
   }
+}
+//判断index是否等于偶数
+if (index == parseInt(tem) * 2) {
+  //当前li元素的背景设置成半格星星状态
+  items[tem].style.background = "url() no-repeat";
+  items[tem].style.backgroundSize = "100% 100%";
+}
+document.getElementById('getStarRate').innerHTML = (parseInt(index) + 1) * 0.5;},
+
 })
